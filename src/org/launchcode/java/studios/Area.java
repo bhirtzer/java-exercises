d0e9e1426002a4d6efe7aa1021f84e77e2656696
Brian Hirtzer <bhirtzer@gmail.com>
2019-03-04 17:43:29 -0600

    Finished the first studio!

---
+++ src/org/launchcode/java/studios/Area.java
@@ -0,0 +1,18 @@
+package org.launchcode.java.studios;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        Scanner in;
+
+        in = new Scanner (System.in);
+        System.out.println("Enter a radius: ");
+        radius = in.nextDouble();
+        area = (Math.PI * radius * radius);
+
+        System.out.println("The area of the circle with a radius of " + radius + " is: " + area);
+    }
+}
