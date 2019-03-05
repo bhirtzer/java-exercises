c7f9d8317803e92f51002d6efee4196fa87998c8
Brian Hirtzer <bhirtzer@gmail.com>
2019-03-04 18:11:11 -0600

    Finished the first studio!

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -6,11 +6,16 @@ public class Area {
     public static void main(String[] args) {
         double radius;
         double area;
-        Scanner in;
+        Scanner in = new Scanner(System.in);
 
-        in = new Scanner (System.in);
         System.out.println("Enter a radius: ");
         radius = in.nextDouble();
+
+        if (radius < 0) {
+            System.out.println("You can't use a negative number!");
+            System.exit(0);
+        }
+
         area = (Math.PI * radius * radius);
 
         System.out.println("The area of the circle with a radius of " + radius + " is: " + area);
